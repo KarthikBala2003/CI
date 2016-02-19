88e898c60078d8e4acc72caa7e5df8f92de95ed4
Karthik Bala <KarthikBala2003@gmail.com>
2016-02-18 20:51:50 -0600

    GitHub:KB:Release Added Java Source File and Batch file

---
+++ com/kb/sample1/CompileMe.bat
@@ -0,0 +1,1 @@
+java -cp D:\Dev\DevGitHub\Java\src\;. com.kb.sample1.HelloWorld
---
+++ com/kb/sample1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.kb.sample1;
+
+public class HelloWorld {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
